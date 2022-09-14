528e6e1ed5aeaca05e77ab83bcda95680fc2e9bd
Mateusz Brzozowski <mateusz.brzozowski@gearcode.eu>
2022-09-14 08:42:41 +0200

    jeszcze serwer i formatowanie

--- index.php
+++ index.php
@@ -1,11 +1,25 @@
 <?php
 echo "<h1>TEST AUTH</h1>";
 
+echo "<pre>";
 var_export('POST');
 var_export($_POST);
+echo "</pre>";
+echo "<br>";
+echo "<pre>";
 var_export('GET');
 var_export($_GET);
-var_export('GET');
+echo "</pre>";
+echo "<br>";
+echo "<pre>";
+var_export('COOKIE');
 var_export($_COOKIE);
+echo "</pre>";
+echo "<br>";
+echo "<pre>";
+var_export('SERVER');
+var_export($_SERVER);
+echo "</pre>";
+echo "<br>";
 
 echo "<br />KONIEC";
